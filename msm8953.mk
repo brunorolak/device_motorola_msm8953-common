129a33b2e4aa1fd19b1840b8220827a9bdec9fba
LuK1337 <priv.luk@gmail.com>
2020-05-16 20:28:31 +0200

    msm8953-common: Build 64-bit variants of android.hardware.camera.{device,provider}
    * Fixes: ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.4.so" not found
    
    Change-Id: I938dd7135a53e9e0951c31e7f56082c0564cf924

--- msm8953.mk
+++ msm8953.mk
@@ -71,9 +71,13 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.5 \
     camera.device@3.2-impl \
+    libbson.vendor \
+    libxml2 \
     libwui \
     libbson.vendor \
     Snap
