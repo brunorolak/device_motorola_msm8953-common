87415a6fc113fef3101dcfbea2a5e329da90c15a
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    msm8953-common: rro_overlays: import RRO CarrierConfig
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- msm8953.mk
+++ msm8953.mk
@@ -388,7 +388,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.1.vendor \
     android.hardware.radio.config@1.0.vendor \
-    android.hardware.radio.deprecated@1.0.vendor
+    android.hardware.radio.deprecated@1.0.vendor \
+    CarrierConfigOverlay
 
 # Telephony
 PRODUCT_PACKAGES += \
