abd90cdf7cef81edb55dc9b19d54b89ee62d9196
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    msm8953-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- msm8953.mk
+++ msm8953.mk
@@ -362,6 +362,9 @@ PRODUCT_PACKAGES += \
     libmemset_shim \
     libqsap_shim
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
