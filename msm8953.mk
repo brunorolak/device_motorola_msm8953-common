7df1140cfe733aefd7f518565c0a9ca3087f9a35
Jeferson <jroliveira.oliveira301@gmail.com>
2021-09-28 19:31:38 +0200

    msm8953-common: Build android.hardware.camera.common@1.0
    E/ims_camera_jni(2657): Error loading library lib-imscamera.so: dlopen failed: library "android.hardware.camera.common@1.0.so" not found
    Change-Id: I274b10a7a7f52c7ffb7c0f13f49cfe07d6418dab

--- msm8953.mk
+++ msm8953.mk
@@ -70,6 +70,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
