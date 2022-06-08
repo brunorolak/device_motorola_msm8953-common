2c943bc7cc4e096047c878b0941ef131a5e4358e
brunorolak <bruno.rolak@gmail.com>
2022-06-08 02:15:04 -0300

    Revert "msm8953-common: defines clang azure"
    This reverts commit 4316cf31f4ec2f2a8a4c40e7ed3c2d75f0d70ef6.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,10 +109,6 @@ BOARD_NO_CHARGER_LED := true
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_LEGACY_HW_DISK_ENCRYPTION := true
 
-# Clang
-TARGET_KERNEL_CLANG_VERSION := azure
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-azure/bin/aarch64-linux-gnu-
-
 # Display
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 TARGET_USES_ION := true
