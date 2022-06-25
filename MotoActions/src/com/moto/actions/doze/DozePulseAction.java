bc5c5c195debd869e4193c8a69b53ffe8f309928
elginsk8r <flintman@nreuter85@gmail.com>
2022-06-25 16:47:28 -0300

    msm8953-common: MotoActions: Doze: Run as user
    This was broadcasting via system and there seems to be more
    restictions.  Shift over to user.
    Changes found in OnePlus Doze

--- MotoActions/src/com/moto/actions/doze/DozePulseAction.java
+++ MotoActions/src/com/moto/actions/doze/DozePulseAction.java
@@ -19,6 +19,7 @@ package com.moto.actions.doze;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserHandle;
 import android.util.Log;
 
 import com.moto.actions.SensorAction;
@@ -48,7 +49,8 @@ public class DozePulseAction implements SensorAction, ScreenStateNotifier {
     public void action() {
          if (mayDoze()) {
             Log.d(TAG, "Sending doze.pulse intent");
-            mContext.sendBroadcast(new Intent("com.android.systemui.doze.pulse"));
+            mContext.sendBroadcastAsUser(new Intent("com.android.systemui.doze.pulse"),
+                new UserHandle(UserHandle.USER_CURRENT));
         }
     }
 
